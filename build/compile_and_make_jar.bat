419771e38f97bcd7f96aeaba3a49d5554ea451d9
Jonathan Koller <koller3@gmail.com>
2017-09-27 12:32:38 -0500

    Update build and test scripts
     - add version info to jar file name

--- build/compile_and_make_jar.bat
+++ build/compile_and_make_jar.bat
@@ -1,9 +1,13 @@
 SET JDK_PATH="C:\Program Files (x86)\Java\jdk1.8.0_141"
-SET JDK_BIN=%JDK_PATH%"\bin"
-SET JAVAC_EXE=%JDK_BIN%"\javac.exe"
-set JAR_EXE=%JDK_BIN%"\jar.exe"
-SET JAR_FILE="Pumpctrl.jar"
-del %JAR_FILE%
+SET JDK_BIN=%JDK_PATH%\bin
+SET JAVAC_EXE=%JDK_BIN%\javac.exe
+SET JAR_EXE=%JDK_BIN%\jar.exe
+SET SRC_FILE=..\src\Pumpctrl.java
+findstr "[0-9]\.[0-9]\.[0-9]-20[1-9][0-9][0-9][0-9][0-9][0-9]" %SRC_FILE% > ver_line.txt
+SET /p VER_LINE=<ver_line.txt
+SET VER_STRING=%VER_LINE:~-17,14%
+SET JAR_FILE=Pumpctrl_%VER_STRING%.jar
+del %JAR_FILE% ver_line.txt
 
 %JAVAC_EXE% -d . ..\src\*.java
 %JAR_EXE% cvfm %JAR_FILE% mainclass.txt *.class
--- build/test_jar.bat
+++ build/test_jar.bat
@@ -1,7 +1,11 @@
 SET JRE_PATH="C:\Program Files (x86)\Java\jre1.8.0_141"
-SET JRE_BIN=%JRE_PATH%"\bin"
-SET JAVA_EXE=%JRE_BIN%"\java.exe"
-SET JAR_FILE="Pumpctrl.jar"
+SET JRE_BIN=%JRE_PATH%\bin
+SET JAVA_EXE=%JRE_BIN%\java.exe
+SET SRC_FILE=..\src\Pumpctrl.java
+findstr "[0-9]\.[0-9]\.[0-9]-20[1-9][0-9][0-9][0-9][0-9][0-9]" %SRC_FILE% > ver_line.txt
+SET /p VER_LINE=<ver_line.txt
+SET VER_STRING=%VER_LINE:~-17,14%
+SET JAR_FILE=Pumpctrl_%VER_STRING%.jar
+del ver_line.txt
 
 %JAVA_EXE% -jar %JAR_FILE%
-
